15d3839470846e4a96fc2d27203ecf4a8f43f06a
Luke Lovett <llovett@oberlin.edu>
2013-01-10 18:17:31 -0500

    box bounds will selectively reveal points within

--- bbox/views.py
+++ bbox/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render_to_response
 from bbox.models import Point
+import json
 
 def addPoint( request ):
     lat, lng = float(request.POST['lat']), float(request.POST['lng'])
@@ -8,6 +9,9 @@ def addPoint( request ):
     return HttpResponse()
 
 def searchPoints( request ):
-    lat, lng = request.GET['lat'], request.GET['lng']
-    # TODO: find points in database
-    return HttpResponse()
+    lat1, lng1 = float(request.GET['lat1']), float(request.GET['lng1'])
+    lat2, lng2 = float(request.GET['lat2']), float(request.GET['lng2'])
+    points = [p.position for p in Point.objects( position__within_box=[(lat1,lng1),(lat2,lng2)] )]
+    
+    return HttpResponse( json.dumps( points ),
+                         mimetype="application/json" )
