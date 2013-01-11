9e27dded4bc4d7f5dc3b0cabc8514d38e3924146
Luke Lovett <llovett@oberlin.edu>
2013-01-11 16:21:52 -0500

    added bounding boxes to route search

--- bbox/views.py
+++ bbox/views.py
@@ -2,6 +2,8 @@ from django.http import HttpResponse
 from django.shortcuts import render_to_response
 from bbox.models import Point
 import json
+from sys import stderr
+from Polygon.Shapes import Rectangle
 
 def addPoint( request ):
     lat, lng = float(request.POST['lat']), float(request.POST['lng'])
@@ -9,9 +11,29 @@ def addPoint( request ):
     return HttpResponse()
 
 def searchPoints( request ):
-    lat1, lng1 = float(request.GET['lat1']), float(request.GET['lng1'])
-    lat2, lng2 = float(request.GET['lat2']), float(request.GET['lng2'])
-    points = [p.position for p in Point.objects( position__within_box=[(lat1,lng1),(lat2,lng2)] )]
-    
-    return HttpResponse( json.dumps( points ),
-                         mimetype="application/json" )
+    postData = json.loads(request.raw_post_data)
+    rectangles = postData['rectangles']
+
+    # For debugging only
+    stderr.write( "received %d rectangles"%(len(rectangles)/4) )
+
+    # bboxArea = the union of all the bounding boxes on the route
+    bboxArea = None
+    for i in xrange(0,len(rectangles),4):
+        # Make a Rectangle out of the width/height of a bounding box
+        theRect = Rectangle( abs(rectangles[i+1] - rectangles[i+3]),
+                             abs(rectangles[i] - rectangles[i+2]) )
+
+        # Translate the rectangle by the SW corner of the box
+        theRect.shift( rectangles[i+2], rectangles[i+3] )
+
+        # DOUBLE the size of the rect!???? This really shouldn't be necessary
+        # theRect.scale(2,2)
+
+        # Union the bounding box
+        bboxArea = bboxArea + theRect if bboxArea else theRect
+
+    # For debugging only --- did it really work??
+    stderr.write( str(bboxArea) )
+
+    return HttpResponse()
